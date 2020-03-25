dd459e9c520f2ca95ffc928c097dbd5795851d01
Jorge Rangel <jorgearturo99@live.com.mx>
2020-03-24 19:48:26 -0600

    Index actualizado

---
+++ Fetch/Practica1/practica1.js
@@ -0,0 +1,36 @@
+var paises = document.getElementById("paises");
+
+fetch("datos.json")
+    .then(function (response) {
+        response.json().then(function (datos) {
+            datos.forEach(registro => {
+                console.log(registro);
+
+                let li = document.createElement("li");
+                let pais = document.createElement("span");
+                let casos = document.createElement("span");
+
+                li.className = "list-group-item";
+                li.setAttribute("id", registro.country);
+
+                pais.innerHTML = registro.country+ " &emsp;";
+                casos.textContent = registro.cases;
+                casos.className = "badge badge-primary badge-pill";
+                li.appendChild(pais);
+                li.appendChild(casos);
+                paises.appendChild(li);
+
+                li.addEventListener("mouseover", function () {
+                    li.className = "list-group-item active";
+                });
+                li.addEventListener("mouseout", function () {
+                    li.className = "list-group-item";
+                });
+            });
+        });
+    })
+    .catch(function (error) {
+        console.log(
+            "Hubo un problema con la petición Fetch:" + error.message
+        );
+    });
