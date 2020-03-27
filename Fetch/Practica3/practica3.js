bbe3f2011c4d14d7a91d27ec4ef2432445df8c90
Jorge Rangel <jorgearturo99@live.com.mx>
2020-03-26 18:56:38 -0600

    Prueba practica 4

--- Fetch/Practica3/practica3.js
+++ Fetch/Practica3/practica3.js
@@ -336,7 +336,7 @@ function iniciaMapa() {
 
     var icono = {
         url: "https://interbolivia.com/wp-content/uploads/2018/12/pulse.gif",
-        scaledSize: new google.maps.Size(10, 10),
+        scaledSize: new google.maps.Size(15, 15),
         origin: new google.maps.Point(0, 0),
         anchor: new google.maps.Point(0, 0)
     }
